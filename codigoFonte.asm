48eb37a8a7a45c41ed234f59c283122da380bfa5
Vinicius <viniciushallmann2015@gmail.com>
2023-04-21 15:46:48 -0300

    commit codigoFonte (incompleto)

--- codigoFonte.asm
+++ codigoFonte.asm
@@ -1,15 +1,14 @@
 .data 
 	#Variaveis menu
-	menu: 		.asciiz "\n-----MENU-----\n (1) Opção 1;\n (2) Opção 2;\n (3) Sair;\n (4) Opção 4;\n (5) Opção 5;\n"
+	menu: 		.asciiz "\n-----MENU-----\n (1) Colocar todas letras em minuscula;\n (2) Colocar todas letras em maiuscula;\n (3) Inverter string\n (4) Capitalizar string;\n (5) Remover espaço(s) de uma string;\n (6) Verificar se é substring;\n (7) Sair;\n"
 	opçãoMenu: 	.asciiz "Digite sua uma opção:"
-	testeVolta: 	.asciiz "TESTE"
-	#Variaveis colocarEmMinusculo e colocarEmMaiusculo
+	#Variaveis para uso da string
 	stringAlterada: .space 100	#Tamanho do espaço de memória para a string
 	entradaString: .space 100  
-	#Variaveis inverteString 
-	stringOpção3: .asciiz "essa string esta invertida" 
-	#Variaveis capitalizar string
-	stringOpção4: .asciiz "Aoi    Essa string esta errada e um teste" 
+	substring: .space 100
+	achou: .asciiz "é substring"
+	nachou: .asciiz "nao é substring"
+	#################
 .text
 	li $s1, 1  			#Opção de saída do menu
 	li $s2, 2  			#Opção de saída do menu
@@ -18,7 +17,7 @@
 	li $s5, 5  			#Opção de saída do menu
 	li $s6, 6  			#Opção de saída do menu
 	li $s7, 7  			#Opção de saída do menu
-LOOPMENU:
+LOOP_MENU:
 	#Print das linhas do MENU
 	li $v0, 4
 	la $a0, menu
@@ -36,11 +35,11 @@ LOOPMENU:
 	beq $v0, $s2, OPÇÃO2	#Se $t1 = 2, pula para a opção 2
 	beq $v0, $s3, OPÇÃO3	#Se $t1 = 3, pula para a opção 3
 	beq $v0, $s4, OPÇÃO4	#Se $t1 = 4, pula para a opção 4
-	#beq $v0, $s5, OPÇÃO5	#Se $t1 = 5, pula para a opção 5
-	#beq $v0, $s6, OPÇÃO6	#Se $t1 = 6, pula para a opção 6
+	beq $v0, $s5, OPÇÃO5	#Se $t1 = 5, pula para a opção 5
+	beq $v0, $s6, OPÇÃO6	#Se $t1 = 6, pula para a opção 6
 	#beq $v0, $s7, OPÇÃO7	#Se $t1 = 7, pula para a opção 7
 	beq $v0, $s7, FIM	#Se $t1 = 7, pula para o fim do program
-	j LOOPMENU	
+	j LOOP_MENU	
 		
 OPÇÃO1:
 	li 	$v0, 8			#Código do syscall de leitura de string
@@ -51,28 +50,28 @@ OPÇÃO1:
 	la $t0, stringAlterada 		#Endereço da stringAlterada
 	la $t1, entradaString		#Salva nesse endenreço a string digitada pelo usuario
 	
-	LOOPOPÇÃO1:
+	LOOP_OPÇÃO1:
 		lbu 	$s0, ($t1)			#Le o primeiro caractere da string no registrador $s1
-		beq 	$s0, $zero, FIMOPÇÃO1		#Verifica se o caractere não é vazio, se for pula para o final do código, se não
-		blt 	$s0, 122, LETRAMINUSCULA	#Se for menor que 97, não é um caracter válido e pula para o proximo caractere
+		beq 	$s0, $zero, FIM_OPÇÃO1		#Verifica se o caractere não é vazio, se for pula para o final do código, se não
+		blt 	$s0, 122, LETRA_MINUSCULA	#Se for menor que 97, não é um caracter válido e pula para o proximo caractere
 
-	LETRAMINUSCULA:
-		blt 	$s0, 65, ITERAÇÃOOPÇÃO1
-		bgt 	$s0, 90, ITERAÇÃOOPÇÃO1
+	LETRA_MINUSCULA:
+		blt 	$s0, 65, ITERAÇÃO_OPÇÃO1
+		bgt 	$s0, 90, ITERAÇÃO_OPÇÃO1
 		addi 	$s0, $s0, 32			#Soma 32 para colocar a letra em minusculo
 		sb 	$s0, ($t0)
-		j ITERAÇÃOOPÇÃO1
-	ITERAÇÃOOPÇÃO1:
+		j ITERAÇÃO_OPÇÃO1
+	ITERAÇÃO_OPÇÃO1:
 		sb 	$s0, ($t0)		#Salva o valor de $s0 em $a0
 		addi 	$t0, $t0, 1		#Incrementa em 1 o valor na memória para ir para o próximo caracter 
 		addi 	$t1, $t1, 1
-		j LOOPOPÇÃO1
+		j LOOP_OPÇÃO1
 	
-	FIMOPÇÃO1:					#imprime a string alterada
+	FIM_OPÇÃO1:					#imprime a string alterada
 		li 	$v0, 4
 		la 	$a0, stringAlterada
 		syscall 
-		j LOOPMENU
+		j LOOP_MENU
 OPÇÃO2: 
 	li 	$v0, 8			#Código do syscall de leitura de string
 	la 	$a0, entradaString	#Salva a string 
@@ -84,84 +83,172 @@ OPÇÃO2:
 	
 	loop:
 		lbu 	$s0, ($t1)			#Le o primeiro caractere da string no registrador $s1
-		beq 	$s0, $zero, FIMOPÇÃO2		#Verifica se o caractere não é vazio, se for pula para o final do código, se não
-		blt 	$s0, 97, LETRAMAIUSCULA		#Se for menor que 97, não é um caracter válido e pula para o proximo caractere
-		bgt 	$s0, 122, ITERAÇÃOOPÇÃO2		#Se for maior que 122, não é um caracter válido e pula para o proximo caractere
+		beq 	$s0, $zero, FIM_OPÇÃO2		#Verifica se o caractere não é vazio, se for pula para o final do código, se não
+		blt 	$s0, 97, LETRA_MAIUSCULA		#Se for menor que 97, não é um caracter válido e pula para o proximo caractere
+		bgt 	$s0, 122, ITERAÇÃO_OPÇÃO2		#Se for maior que 122, não é um caracter válido e pula para o proximo caractere
 		addi 	$s0, $s0, -32			#Substrai -32 para colocar a letra em maiusculo
 		sb 	$s0, ($t0)			#Salva o valor de $s0 em $a0
-		j ITERAÇÃOOPÇÃO2				#Pula para a interação
+		j ITERAÇÃO_OPÇÃO2				#Pula para a interação
 	
-	LETRAMAIUSCULA:
-		blt 	$s0, 65, ITERAÇÃOOPÇÃO2		#Se for uma letra entre 65 e 90 na tabela ASCII
-		bgt 	$s0, 90, ITERAÇÃOOPÇÃO2		#quer dizer que a letra já é maiuscula, então eu pulo ela indo direto para iteração
+	LETRA_MAIUSCULA:
+		blt 	$s0, 65, ITERAÇÃO_OPÇÃO2		#Se for uma letra entre 65 e 90 na tabela ASCII
+		bgt 	$s0, 90, ITERAÇÃO_OPÇÃO2		#quer dizer que a letra já é maiuscula, então eu pulo ela indo direto para iteração
 		sb 	$s0, ($t0)			#Salva o valor de $s0 em $t0
-		j ITERAÇÃOOPÇÃO2
+		j ITERAÇÃO_OPÇÃO2
 	
-	ITERAÇÃOOPÇÃO2:
+	ITERAÇÃO_OPÇÃO2:
 		addi 	$t0, $t0, 1		#Incrementa em 1 o valor na memória para ir para o próximo caracter 
 		addi 	$t1, $t1, 1
 		j loop
 	
-	FIMOPÇÃO2:					#imprime a string alterada
+	FIM_OPÇÃO2:			#imprime a string alterada
 		li 	$v0, 4
 		la 	$a0, stringAlterada
 		syscall
-		j LOOPMENU
+		j LOOP_MENU
 OPÇÃO3:
-	#A FAZER, A PESSOA ENTRA COM UMA STRING QUALQUER
-	la $a0 , stringOpção3 # string ad
-	lb $t1, 0($a0) # t1 string letter
-	ADDINSTACK:
-		beq $t1, 0, DESTACK # if letter = null
-		sb $t1, ($sp) # store letter in stack
-		addiu $sp, $sp, -4 # increase stack
-		addi $a0, $a0, 1 # increase string ad
-		lb $t1, 0 ($a0) # load string letter
-		j ADDINSTACK # main loop
+	li 	$v0, 8			#Código do syscall de leitura de string
+	la 	$a0, entradaString	#Salva a string 
+	li	$a1, 100		#Tamanho da string
+	syscall
+	lb $t1, 0($a0) 			#Salva em $t1 o primeiro caractere de $a0
+	ADD_IN_STACK:
+		beq $t1, 0, DESTACK 	#Se a letra for nula, fula para o DESTACK
+		sb $t1, ($sp) 		#Coloca a letra na pilha
+		addiu $sp, $sp, -4 	#Vai para a próxima posição da pilha
+		addi $a0, $a0, 1 	#Incrementa em 1 o valor de endereço da string (vai para o próximo caractere)
+		lb $t1, 0 ($a0) 	#Da um load do caractere em $t1
+		j ADD_IN_STACK 		#volta para o loop
 	
 	DESTACK:
-		la $a0, stringOpção3
-		addiu $sp, $sp, 4
-	DESTACKLOOP: 
-		lb $t1, 0($sp)
-		beq $t1, 0, FIMOPÇÃO3
-		sb $t1, ($a0)
-		addiu $sp, $sp, 4
-		addi $a0, $a0, 1
-		j DESTACKLOOP
-	FIMOPÇÃO3:
+		la $a0, stringAlterada	#Da um load da string
+		addiu $sp, $sp, 4	#Avança para o valor anterior da pilha
+		
+	DESTACK_LOOP: 
+		lb $t1, 0($sp)		#Pega o caractere do topo da pilha
+		beq $t1, 0, FIM_OPÇÃO3	#Se o caractere do topo for nulo, pula para o FIMOPÇÃO3
+		sb $t1, ($a0)		#Salva o caractere em $t1
+		addiu $sp, $sp, 4	#Vai para o valor anterior do topo da pilha
+		addi $a0, $a0, 1	#Incrementa em 1 o endereço da string para ir para o próximo caractere
+		j DESTACK_LOOP
+		
+	FIM_OPÇÃO3:			#imprime a string alterada
 		li $v0, 4
-		la $a0, stringOpção3
+		la $a0, stringAlterada
 		syscall
-		j LOOPMENU
+		j LOOP_MENU
 OPÇÃO4:
-	addi $a1, $a1, 32 # a1 espaco
-	la $a0, stringOpção4 # a0 endereco da string
-	lb $t8, 0($a0) # t8 primeira letra
-	sle $t7, $t8, 90  # ja é MAIUSCULO
-	beq $t7, 1, LOOPOPÇÃO4
-	addi $t8, $t8, -32 # transforma a primeira letra em maiusculo
-	sb $t8, 0($a0) # salva a letra na string
-	LOOPOPÇÃO4:	
-		addi $a0 , $a0, 1
-		lb $t8, 0($a0) 
-		beq $t8, $zero, FIMOPÇÃO4
-		beq $t8, $a1, ESPACO
-		j LOOPOPÇÃO4
-	ESPACO:
-		addi $a0 , $a0, 1 #dpois do espaco 
-	MAIUSCULO:
-		lb $t3, 0($a0) #t3 load letra
-		sle $t7, $t3, 90  # ja é MAIUSCULO
-		beq $t7, 1, LOOPOPÇÃO4  # se for maiusculo volta pro loop
-		beq $t3, 32, LOOPOPÇÃO4 # se proximo e espaco verifica de novo
-		add $t3, $t3, -32 # transforma
-		sb $t3, 0($a0) #t9 store letra um endereco atras
-	j LOOPOPÇÃO4
-	FIMOPÇÃO4:
+	li 	$v0, 8			#Código do syscall de leitura de string
+	la 	$a0, entradaString	#Salva a string no endereço $a0
+	li	$a1, 100		#Tamanho da string
+	syscall
+	addi $a3, $a3, 32 	#a3 recebe o valor do caractere espaco
+	
+	lb $t8, 0($a0) 		#t8 recebe a primeira letra
+	sle $t7, $t8, 90  	#Se já for MAIUSCULO, set $t1 com 1
+	beq $t7, 1, LOOP_OPÇÃO4	#Se $t1 for == 1, então pula para a LOOPOPÇÃO4
+	addi $t8, $t8, -32 	#Se não pular, então transforma a primeira letra em maiusculo
+	sb $t8, 0($a0) 		#Salva a letra na string
+	
+	LOOP_OPÇÃO4:	
+		addi $a0 , $a0, 1		#Soma 1 em $a0 para pegar o primeiro caractere 
+		lb $t8, 0($a0) 			#Salva esse caractere em $t8
+		beq $t8, $zero, FIM_OPÇÃO4	#Se $t8 for nulo, quer dizer que chegou no final da string e pula para o FIMOPÇÃO4
+		beq $t8, $a3, ESPACO_OPÇÃO4	#Se não, testa se o caractere é == espaco ($a1)
+		j LOOP_OPÇÃO4			#Volta pro loop
+		
+	ESPACO_OPÇÃO4:
+		addi $a0 , $a0, 1 		#Soma um em $a0 para ir para o próximo caractere (Pula o espaço no caso)
+		
+	MAIUSCULO_OPÇÃO4:
+		lb $t3, 0($a0) 			#$t3 recebe a letra
+		sle $t7, $t3, 90  		#Se já for MAIUSCULO, set $t7 com 1
+		beq $t7, 1, LOOP_OPÇÃO4  	#Se for maiusculo volta pro loop
+		beq $t3, 32, LOOP_OPÇÃO4 	#Se for um espaco verifica de novo
+		add $t3, $t3, -32 		#Transforma em maiusculo
+		sb $t3, 0($a0) 			#$t3 recebe uma letra de um endereco atras
+		j LOOP_OPÇÃO4			#Volta pro loop
+		
+	FIM_OPÇÃO4:				#imprime a string alterada
 		li $v0 , 4
-		la $v1 , stringOpção4
-		la, $a0, stringOpção4
+		la, $a0, entradaString
 		syscall
-		j LOOPMENU
+		j LOOP_MENU
+OPÇÃO5:
+	li 	$v0, 8			#Código do syscall de leitura de string
+	la 	$a0, entradaString	#Salva a string no endereço $a0
+	li	$a1, 100		#Tamanho da string
+	syscall
+	addi $a3, $a3, 32 	#$a3 recebe o valor do caractere espaco
+	LOOP_OPÇÃO5:	
+		lb $t8, 0($a0) 			#%t8 recebe um caractere
+		beq $t8, $zero, FIM_OPÇÃO5	#Se %t8 for nulo pula para o FIMOPÇÃO5
+		beq $t8, $a3, ESPACO_OPÇÃO5	#Se for um espaço pula para a label ESPACOOPÇÃO5
+		addi $a0 , $a0, 1		#Incrementa em 1 o endereço da string para o próximo caractere
+		j LOOP_OPÇÃO5
+	
+	ESPACO_OPÇÃO5:
+		la $t4, 0($a0) 	#$t4 recebe o endereço do espaco
+		la $t3, 1($t4) #$t3 recebe o endereço depois do espaco
+	
+	ESPACO_LOOP_OPÇÃO5:
+		lb $t9, 0($t3) 		#$t9 recebe o endereço depois do espaço
+		sb $t9, 0($t4) 		#$t9 store letra um endereco atras
+		addi $t3, $t3, 1	#Incrementa em um o endereço da string
+		addi $t4, $t4, 1
+		beq $t9, $zero, LOOP_OPÇÃO5 	#Se $t9 for nulo pula para o LOOPOPÇÃO5
+		j ESPACO_LOOP_OPÇÃO5		#Se não continua nesse loop salvando procurando por espaço e salvando o valor da frente do espaço
+	
+	FIM_OPÇÃO5:				#imprime a string alterada
+		li $v0 , 4
+		la $a0, entradaString
+		syscall
+		j LOOP_MENU
+OPÇÃO6:
+	li 	$v0, 8			#Código do syscall de leitura de string
+	la 	$a0, entradaString	#Salva a string no endereço $a0
+	li	$a1, 100		#Tamanho da string
+	syscall 
+	lb $t0, ($a0)
+	li 	$v0, 8			#Código do syscall de leitura de string
+	la 	$a2, substring		#Salva a string no endereço $a0
+	li	$a1, 100		#Tamanho da string
+	syscall
+	lb $t1, ($a2)		#Salva em $t1 o primeiro caractere de $a2 (substring)
+	PERCORRE_STRING:
+		lb $t0, ($a0)				#Salva em $t0 um caractere de $a0
+		beq $t0, $t1, PRIMEIRA_LETRA_IGUAL	#Se o caractere de $t0 for igual ao de $t1 pula para o PRIMEIRA_LETRA_IGUAL
+		beq $t0, 0, NAO_É_SUBSTRING		#Se $t0 for nulo, pula para não é uma substring
+		addi $a0, $a0, 1 
+		j PERCORRE_STRING
+	PRIMEIRA_LETRA_IGUAL:
+		la $a3, 1($a0)		#Da um load do caractere em $ao para $a3
+	ACHOU:				
+		addi $a0, $a0, 1 	#Incrementa para o próximo caractere
+		addi $a2, $a2, 1	#Incrementa para o próximo caractere
+		lb $t0, ($a0)		#Da um load do caractere em $t0
+		lb $t1, ($a2)		#Da um load do caractere em $t1
+		beq $t1, 0, É_SUBSTRING		#Se chegou no final da string pula para É_SUBSTRING
+		beq $t0, 0, NAO_É_SUBSTRING	#Se o caractere em $t0 é nulo, pula para NÃO_É_SUBSTRING
+		bne $t0, $t1, NÃO_ACHOU		#Se %t0 e $t1 são diferente pula para NÃO_ACHOU
+		j ACHOU				#Loop do achou
+		
+	NÃO_ACHOU:
+		la $a0, 0($a3)
+		la $a2, substring
+		lb $t1, ($a2)
+		j PERCORRE_STRING	
+	
+	NAO_É_SUBSTRING:
+		li $v0, 4
+		la $a0, nachou
+		syscall
+		li $v0, 10
+		syscall
+	É_SUBSTRING:
+		li $v0, 4
+		la $a0, achou
+		syscall
+		li $v0, 10
+		syscall
 FIM:
